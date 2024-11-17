0f73ac7a9991b352548927b0acd00b2b64abd2d8
southixa10 <southixa.pele10@gmail.com>
2024-11-17 15:56:09 +0700

    update: update upload image

--- src/components/UploadImage.jsx
+++ src/components/UploadImage.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { FaTimes } from "react-icons/fa";
 import { CiCamera } from 'react-icons/ci'
 
 const UploadImage = () => {
@@ -8,20 +9,50 @@ const UploadImage = () => {
 
     function handleFileChange (e) {
         console.log("event =>", e);
-
+        const selectedFile = e.target.files[0];
+        setFile(selectedFile);
     }
 
+    function handleRemoveFile () {
+        setFile(null);
+        setPreview(null);
+    }
+
+    useEffect(() => {
+        if(file && file.type.startWith('image/')){
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                setPreview(reader.result);
+            }
+            reader.readAsDataURL(file);
+        } else {
+            setPreview(null);
+        }
+    }, [file])
+
   return (
-    <div>
-        <label for="upload">
-            <div className='w-[150px] h-[150px] bg-gray-200 rounded-md flex justify-center items-center'>
-                <div className='w-[100px] h-[100px] border-gray-300 rounded-sm border-dashed border-[3px] flex justify-center items-center'>
-                    <CiCamera className='text-[48px] text-gray-300' />
+    <>
+        {!preview && (
+            <div>
+                <label for="upload">
+                    <div className='w-[150px] h-[150px] bg-gray-200 rounded-md flex justify-center items-center'>
+                        <div className='w-[100px] h-[100px] border-gray-300 rounded-sm border-dashed border-[3px] flex justify-center items-center'>
+                            <CiCamera className='text-[48px] text-gray-300' />
+                        </div>
+                    </div>
+                </label>
+                <input onChange={(e) => handleFileChange(e)} id="upload" type='file' />
+            </div>
+        )}
+        {preview && (
+            <div className='w-[150px] h-[150px] bg-gray-200 rounded-md relative'>
+                <img src="" alt="preview" className='w-full h-full object-cover' />
+                <div onClick={() => {handleRemoveFile()}} className='w-[24px] h-[24px] bg-gray-400 absolute top-[8px] right-[8px] flex justify-center items-center'>
+                    <FaTimes />
                 </div>
             </div>
-        </label>
-        <input onChange={(e) => handleFileChange(e)} id="upload" type='file' />
-    </div>
+        )}
+    </>
   )
 }
 
