058b0bc6e2d2d5e061ae34fb8a746109844c5b0a
southixa10 <southixa.pele10@gmail.com>
2024-11-17 15:59:42 +0700

    fix: upload image

--- src/components/UploadImage.jsx
+++ src/components/UploadImage.jsx
@@ -19,7 +19,7 @@ const UploadImage = () => {
     }
 
     useEffect(() => {
-        if(file && file.type.startWith('image/')){
+        if(file && file.type.startsWith('image/')){
             const reader = new FileReader();
             reader.onloadend = () => {
                 setPreview(reader.result);
@@ -35,19 +35,19 @@ const UploadImage = () => {
         {!preview && (
             <div>
                 <label for="upload">
-                    <div className='w-[150px] h-[150px] bg-gray-200 rounded-md flex justify-center items-center'>
+                    <div className='cursor-pointer w-[150px] h-[150px] bg-gray-200 rounded-md flex justify-center items-center'>
                         <div className='w-[100px] h-[100px] border-gray-300 rounded-sm border-dashed border-[3px] flex justify-center items-center'>
                             <CiCamera className='text-[48px] text-gray-300' />
                         </div>
                     </div>
                 </label>
-                <input onChange={(e) => handleFileChange(e)} id="upload" type='file' />
+                <input className='hidden' onChange={(e) => handleFileChange(e)} id="upload" type='file' />
             </div>
         )}
         {preview && (
             <div className='w-[150px] h-[150px] bg-gray-200 rounded-md relative'>
-                <img src="" alt="preview" className='w-full h-full object-cover' />
-                <div onClick={() => {handleRemoveFile()}} className='w-[24px] h-[24px] bg-gray-400 absolute top-[8px] right-[8px] flex justify-center items-center'>
+                <img src={preview} alt="preview" className='w-full h-full object-cover' />
+                <div onClick={() => {handleRemoveFile()}} className='cursor-pointer w-[24px] h-[24px] bg-gray-400 absolute top-[8px] right-[8px] flex justify-center items-center'>
                     <FaTimes />
                 </div>
             </div>
