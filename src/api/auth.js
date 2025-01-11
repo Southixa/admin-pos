d45b21af5ca6b3de1173af83a9e5f1e5f7cf44da
southixa10 <southixa.pele10@gmail.com>
2025-01-11 14:14:47 +0700

    add: auth api

---
+++ src/api/auth.js
@@ -0,0 +1,19 @@
+import axios from "axios"
+import ApiPath from "./api.path"
+
+export const LoginApi = async (username, password) => {
+    const config = {
+        headers: {
+            "Content-Type:": "application/json"
+        }
+    }
+    const data = {username, password}
+    try {
+        const response = await axios.post(ApiPath.userLogin, data, config)
+        console.log("Res in LoginApi => ", response?.data);
+        return response?.data
+    } catch (error) {
+        console.log("Error Occurred In LoginApi => ", error);
+        return undefined;
+    }
+}
