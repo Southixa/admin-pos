0b68dd73f936a90a87eb730be57417cb9466ef50
southixa10 <southixa.pele10@gmail.com>
2025-01-12 13:48:19 +0700

    add: getone, del, update category api

--- src/api/category.js
+++ src/api/category.js
@@ -26,3 +26,39 @@ export const GetAllCategoriesApi = async () => {
         return undefined;
     }
 }
+
+export const DeleteCategoryApi = async (id) => {
+    try {
+        const response = await axios.delete(`${ApiPath.deleteCategory}/${id}`, getHeaderConfig())
+        console.log("Res in GetAllCategoriesApi => ", response?.data?.data);
+        return response?.data?.data;
+    } catch (error) {
+        console.log("Error Occurred In DeleteCategoryApi => ", error);
+        return undefined;
+    }
+}
+
+export const GetOneCategoryApi = async (id) => {
+    try {
+        const response = await axios.get(`${ApiPath.getOneCategory}/${id}`, getHeaderConfig())
+        console.log("Res in GetOneCategoryApi => ", response?.data?.data);
+        return response?.data?.data;
+    } catch (error) {
+        console.log("Error Occurred In GetOneCategoryApi => ", error);
+        return undefined;
+    }
+}
+
+export const UpdateCategoryApi = async (id, name, icon) => {
+    try {
+        const formData = new FormData();
+        formData.append('name', name);
+        formData.append('icon', icon, "icon");
+        const response = await axios.put(`${ApiPath.updateCategory}/${id}`, formData, getHeaderConfig("multipart/form-data"))
+        console.log("Res in UpdateCategoryApi => ", response?.data);
+        return response?.data
+    } catch (error) {
+        console.log("Error Occurred In UpdateCategoryApi => ", error);
+        return undefined;
+    }
+}
