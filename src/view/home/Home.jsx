17a423893b19054d38df7d32432a3c63155106a8
southixa10 <southixa.pele10@gmail.com>
2025-01-18 15:21:41 +0700

    add: filter product by category id

--- src/view/home/Home.jsx
+++ src/view/home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import Sidebar from "../../components/Sidebar";
 import noodles from "../../assets/images/noodles.png";
 import { FaBorderAll } from "react-icons/fa";
@@ -8,6 +8,7 @@ import MenuDrawer from "../../components/MenuDrawer";
 import { GetAllCategoriesApi } from "../../api/category";
 import Swal from "sweetalert2";
 import { GetAllProductsApi } from "../../api/product";
+import { formatCurrency } from "../../helpers";
 
 const Home = () => {
 
@@ -15,6 +16,13 @@ const Home = () => {
   const [loading, setLoading] = useState(false)
 
   const [products, setProducts] = useState([])
+
+  const [selectedCategoryId, setSelectedCategoryId] = useState("")
+
+  const filteredProductsByCategoryId = useMemo(() => {
+      if(selectedCategoryId === "") return products
+      return products.filter((product) => product.categoryID === selectedCategoryId)
+  }, [products, selectedCategoryId])
 
   const getData = async () => {
       setLoading(true);
@@ -40,36 +48,12 @@ const Home = () => {
       getData();
   }, [])
 
-
-  const productList = [
-    {
-      image: burger,
-      name: "ຊຸດຂາໄກ່",
-      price: 50000,
-    },
-    {
-      image: burger,
-      name: "ຊຸດຂາໄກ່",
-      price: 50000,
-    },
-    {
-      image: burger,
-      name: "ຊຸດຂາໄກ່",
-      price: 50000,
-    },
-    {
-      image: burger,
-      name: "ຊຸດຂາໄກ່",
-      price: 50000,
-    }
-  ]
-
   return (
     <Sidebar>
       <MenuDrawer />
       <div className="w-[1200px] min-h-[80px] overflow-x-scroll">
         <div className="flex gap-2">
-          <div className="min-w-[130px] bg-white rounded-2xl overflow-hidden shadow-sm">
+          <div onClick={() => setSelectedCategoryId("")} className={`min-w-[130px] ${selectedCategoryId === "" ? "bg-green-200" : "bg-white"} rounded-2xl overflow-hidden shadow-sm`}>
               <div className="w-full h-[90px] pt-[20px] pl-[20px]">
                 <div className="w-[60px] h-[60px] flex justify-center items-center">
                   <FaBorderAll className="text-[40px] text-gray-600" />
@@ -80,7 +64,7 @@ const Home = () => {
               </div>
             </div>
           {categories.map((item, index) => (
-            <div key={index} className="min-w-[130px] bg-white rounded-2xl overflow-hidden shadow-sm">
+            <div onClick={() => setSelectedCategoryId(item.categoryID)} key={index} className={`min-w-[130px] ${selectedCategoryId === item.categoryID ? "bg-green-200" : "bg-white"} rounded-2xl overflow-hidden shadow-sm`}>
               <div className="w-full h-[90px] pt-[20px] pl-[20px]">
                 <div className="w-[60px] h-[60px]">
                   <img
@@ -99,13 +83,13 @@ const Home = () => {
       </div>
 
         <div className="w-full mt-[20px] grid grid-cols-12 gap-10">
-          {productList.map((item, index) => (
+          {products.map((item, index) => (
             <div key={index} className="col-span-3 w-full min-h-[300px] bg-white rounded-2xl shadow-md p-[16px]">
               <div className="w-full h-[160px] bg-gray-200 rounded-md">
                 <img src={item.image} alt="product" className="w-full h-full object-cover rounded-md" />
               </div>
               <p className="text-[20px] mt-2">{item.name}</p>
-              <p className="mt-1 text-green-600">{item.price} ກີບ</p>
+              <p className="mt-1 text-green-600">{formatCurrency(item.price)} ກີບ</p>
               <button className="w-full bg-green-400 rounded-md py-2 mt-2">ເພີ່ມເມນູ</button>
             </div>
           ))}
