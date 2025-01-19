260250390cb8dd7731ea12ce67323544585caba8
southixa10 <southixa.pele10@gmail.com>
2025-01-19 13:58:11 +0700

    add: add sale deetail api

---
+++ src/api/saleDetail.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { getHeaderConfig } from "../helpers";
+import ApiPath from "./api.path";
+
+export const AddSaleDetailApi = async (menuID, saleID, amount) => {
+    const data = {
+        menuID,
+        saleID,
+        amount
+    }
+    try {
+        const response = await axios.post(ApiPath.insertSale, data, getHeaderConfig("multipart/form-data"))
+        console.log("Res in AddSaleDetailApi => ", response?.data);
+        return response?.data
+    } catch (error) {
+        console.log("Error Occurred In AddSaleDetailApi => ", error);
+        return undefined;
+    }
+}
