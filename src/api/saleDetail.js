7e4f641aecf7b4faffb8283a8ae7adfa23c399d2
southixa10 <southixa.pele10@gmail.com>
2025-01-19 16:13:39 +0700

    add: getAllsaleDetailsBySaleId api

--- src/api/saleDetail.js
+++ src/api/saleDetail.js
@@ -17,3 +17,14 @@ export const AddSaleDetailApi = async (menuID, saleID, amount) => {
         return undefined;
     }
 }
+
+export const GetAllSaleDetailsBySaleIdApi = async (saleID) => {
+    try {
+        const response = await axios.get(`${ApiPath.getAllSaleDetailsBySaleId}${saleID}`, getHeaderConfig())
+        console.log("Res in GetAllSaleDetailsBySaleIdApi => ", response?.data?.data);
+        return response?.data?.data;
+    } catch (error) {
+        console.log("Error Occurred In GetAllSaleDetailsBySaleIdApi => ", error);
+        return undefined;
+    }
+}
